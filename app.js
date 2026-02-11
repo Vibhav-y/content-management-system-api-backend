b11e0e56e6a39defc8b4c0b09d62f68996e32425
Vibhav Yadav <vibhavydm@gmail.com>
2026-02-11 15:33:13 +0530

    cors fix

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 /* Middlewares */
-app.use(cors());
+app.use(cors({
+  origin: true, // Reflects the request origin, allowing unlimited origins with credentials
+  credentials: true
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(morgan("dev"));
