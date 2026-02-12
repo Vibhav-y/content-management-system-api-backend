f3360051bee4549cb54a9dc17ac4ec826372fb2d
Vibhav Yadav <vibhavydm@gmail.com>
2026-02-12 11:12:47 +0530

    reverse

---
+++ app.js
@@ -0,0 +1,51 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import authRoutes from "./routes/auth.route.js";
+import artifactRoutes from "./routes/artifacts.route.js"
+import likes from "./routes/likes.routes.js";
+import comment from "./routes/comment.route.js";
+import cookieParser from "cookie-parser";
+import { testing } from "./crons/testing.js";
+import webhookRoutes from "./webhook/webhooks.js"
+import chatRoutes from "./routes/chats.route.js"
+const app = express();
+
+
+/* Middlewares */
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(morgan("dev"));
+
+
+testing();
+
+
+app.use(cookieParser());
+/* Test Route */
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "CMS Backend is running"
+  });
+});
+
+
+
+app.use("/webhooks", webhookRoutes);
+app.use("/auth",authRoutes);
+app.use("/artifacts", artifactRoutes);
+app.use("/likes", likes);
+app.use("/comments", comment);
+app.use("/chats", chatRoutes);
+
+export default app;
+
+
+
+
+// app.use(cors({
+//   origin: ["https://cms-admin.vercel.app"],
+//   credentials: true
+// }));
